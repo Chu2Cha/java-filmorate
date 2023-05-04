fb1028149b9178172af3db2a1deb658db1197fce
Boris Chuguev <chuguevb@gmail.com>
2023-05-04 11:41:46 +0300

    Users - full validation (base)

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,4 +1,79 @@
 package ru.yandex.practicum.filmorate.controller;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class UserController {
+    private int id =1;
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+    private final List<User> users = new ArrayList<>();
+
+    @GetMapping("/users")
+    public List<User> findAll() {
+        log.debug("Количество пользователей: {}", users.size());
+        return users;
+    }
+
+    @PostMapping("/users")
+    public User create(@RequestBody User user) {
+        for (User addedUser : users) {
+            if (user.equals(addedUser)) {
+                throw new ValidationException("Пользователь с таким email уже существует!");
+            }
+        }
+        User newUser = userValidation(user);
+        newUser.setId(id++);
+        users.add(newUser);
+        log.debug("Пользователь: {}", newUser);
+        return newUser;
+    }
+
+    @PutMapping("/users")
+    public User put(@RequestBody User user) {
+        boolean checkUserforUpdate = false;
+        for (int i = 0; i < users.size(); i++) {
+            if(userValidation(user).equals(users.get(i))){
+                users.set(i, userValidation(user));
+                checkUserforUpdate = true;
+            }
+        }
+        if(!checkUserforUpdate){
+            throw new ValidationException("Пользователь с id " + user.getId() + " не найден.");
+        }
+        log.debug("Обновленный пользователь: {}", user);
+        return user;
+    }
+
+    private User userValidation(User user){
+        if(user.getEmail().isEmpty()){
+            throw new ValidationException("Почта не может быть пустой!");
+        }
+        if(!(user.getEmail().contains("@"))){
+            throw new ValidationException("Почта должна содержать символ @.");
+        }
+        if(user.getLogin().isEmpty()){
+            throw new ValidationException("Логин не может быть пустым!");
+        }
+        if(user.getLogin().contains(" ")){
+            throw new ValidationException("Логин не может содержать пробелы!");
+        }
+        if(user.getBirthday().isAfter(LocalDate.now())){
+            throw new ValidationException("Дата рождения должна быть раньше сегодняшнего дня.");
+        }
+        if(user.getName() == null || user.getName().isEmpty()){
+            user.setName(user.getLogin());
+        }
+        return user;
+    }
+
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Data
 public class User {
@@ -11,4 +12,17 @@ public class User {
     private String login;
     private String name;
     private LocalDate birthday;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
