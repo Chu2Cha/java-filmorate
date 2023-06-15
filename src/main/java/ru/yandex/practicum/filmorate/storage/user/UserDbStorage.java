32d4752bc03d70e8e4fc710e1f7452471e286062
Chu2Cha <chuguevb@gmail.com>
2023-06-15 22:34:37 +0300

    FZ-11 ver 0.2

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
@@ -1,11 +1,19 @@
 package ru.yandex.practicum.filmorate.storage.user;
 
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.User;
 
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
+
+@Primary
 public class UserDbStorage implements UserStorage{
     private final JdbcTemplate jdbcTemplate;
 
@@ -13,19 +21,28 @@ public class UserDbStorage implements UserStorage{
         this.jdbcTemplate = jdbcTemplate;
     }
 
+
     @Override
     public User createUser(User user) {
-        jdbcTemplate.update("INSERT INTO USERS VALUES ( 1, ?,?,?,? )",
-                user.getEmail(),user.getLogin(),user.getName(),user.getBirthday());
-        User returnUser = jdbcTemplate.query("SELECT USER_ID FROM USERS WHERE LOGIN = ?", new BeanPropertyRowMapper<>(User.class))
-                .stream().findAny().orElse(null);
-        user.setId(returnUser.getId());
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement
+                    ("INSERT INTO USERS (EMAIL, LOGIN, NAME, BIRTHDAY) VALUES (?, ?, ?, ?)",
+                            Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, user.getEmail());
+            ps.setString(2, user.getLogin());
+            ps.setString(3, user.getName());
+            ps.setDate(4, Date.valueOf(user.getBirthday()));
+            return ps;
+        }, keyHolder);
+        user.setId(keyHolder.getKey().intValue());
         return user;
     }
 
     @Override
     public void removeUser(int userId) {
-
+        jdbcTemplate.update("DELETE FROM USERS WHERE USER_ID = ?",userId);
     }
 
     @Override
